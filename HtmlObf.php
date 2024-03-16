7f27436b5984827aa0620c41ed3f1af6216975f2
NeMeow <arcy.all@yahoo.com>
2024-03-16 19:53:23 +0300

    emoji and cyrillic support

--- HtmlObf.php
+++ HtmlObf.php
@@ -1,28 +1,32 @@
 <?php
 namespace HidedHtml;
 class Obfuscator {
+    protected string $encoding;
+    function __construct($encoding = 'UTF-8') {
+	   $this->encoding = $encoding;
+    }
     private static function __refreshRandom() {
         srand(random_int(0, 9999999) % time());
     }
     private function __splitAtRandom($str)  {
         $this::__refreshRandom();
-        $length = mb_strlen($str, 'UTF-8');
+        $length = mb_strlen($str, encoding: $this->encoding);
         $randomIndex = rand(0, $length - 1);
-        $firstPart = mb_substr($str, 0, $randomIndex, 'UTF-8');
-        $secondPart = mb_substr($str, $randomIndex, encoding: 'UTF-8');
+        $firstPart = mb_substr($str, 0, $randomIndex, encoding: $this->encoding);
+        $secondPart = mb_substr($str, $randomIndex, encoding: $this->encoding);
         return [$firstPart, $secondPart];
     }
     private function __randomString($length = 10) {
         $this::__refreshRandom();
         $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = mb_strlen($characters, encoding: 'UTF-8');
+        $charactersLength = mb_strlen($characters, encoding: $this->encoding);
         $randomString = '';
         for ($i = 0; $i < $length; $i++)
             $randomString .= $characters[rand(0, $charactersLength - 1)];
         return $randomString;
     }
     private function __splitStringRandomLength($string) {
-        $array = mb_str_split($string, encoding: 'UTF-8');
+        $array = mb_str_split($string, encoding: $this->encoding);
         $result = [];
         while (count($array) > 0) {
             $this::__refreshRandom();
